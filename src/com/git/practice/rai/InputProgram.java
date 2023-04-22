bb8a01c9c2468c26df16d211e58cb41359c89849
mrai1127 <mausamrai11@gmail.com>
2023-04-21 22:34:38 -0400

    initial commit

---
+++ src/com/git/practice/rai/InputProgram.java
@@ -0,0 +1,30 @@
+package com.git.practice.rai;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InputProgram {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> results = new ArrayList<>();
+		for(int i = 0; i < 5 ; i++) {
+		
+		
+		System.out.println("What is your name ?");
+		String name = scanner.nextLine();
+		
+		System.out.println("Where are you from?");
+		String location = scanner.nextLine();
+		
+		String result = "Name: " + name + " " + "Location: " + location;
+		results.add(result);
+		}
+		
+		//print the rsults outside the loop
+		for(String result : results) {
+		System.out.println(result);
+		}
+	}
+
+}
